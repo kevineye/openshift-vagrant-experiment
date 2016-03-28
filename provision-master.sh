cf5e8c014e52e9e5bfbb587a6c3e288422d1b537
Kevin Eye <eye@dhcp020-194-139.wireless.buffalo.edu>
2016-03-28 13:49:00 -0400

    fixes

--- provision-master.sh
+++ provision-master.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -ex
 
 yum -y install https://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
 sed -i -e "s/^enabled=1/enabled=0/" /etc/yum.repos.d/epel.repo
@@ -10,37 +11,15 @@ su vagrant
 cd /home/vagrant
 git clone https://github.com/openshift/openshift-ansible
 
-ssh-keygen -f ~/.ssh/id_rsa -N ''
+ssh-keygen -f /home/vagrant/.ssh/id_rsa -N ''
 
 ssh-keyscan master.lan >> ~/.ssh/known_hosts
-sshpass -p vagrant ssh-copy-id -i ~/.ssh/id_rsa.pub master.lan
+sshpass -p vagrant ssh-copy-id -i /home/vagrant/.ssh/id_rsa.pub master.lan
 
 ssh-keyscan node1.lan >> ~/.ssh/known_hosts
-sshpass -p vagrant ssh-copy-id -i ~/.ssh/id_rsa.pub node1.lan
+sshpass -p vagrant ssh-copy-id -i /home/vagrant/.ssh/id_rsa.pub node1.lan
 
 ssh-keyscan node2.lan >> ~/.ssh/known_hosts
-sshpass -p vagrant ssh-copy-id -i ~/.ssh/id_rsa.pub node2.lan
-#!/bin/sh
+sshpass -p vagrant ssh-copy-id -i /home/vagrant/.ssh/id_rsa.pub node2.lan
 
-yum -y install https://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
-sed -i -e "s/^enabled=1/enabled=0/" /etc/yum.repos.d/epel.repo
-yum -y --enablerepo=epel install ansible pyOpenSSL
-
-cp /home/vagrant/sync/ansible-hosts /etc/ansible/hosts
-
-su vagrant
-cd /home/vagrant
-git clone https://github.com/openshift/openshift-ansible
-
-ssh-keygen -f ~/.ssh/id_rsa -N ''
-
-ssh-keyscan master.lan >> ~/.ssh/known_hosts
-sshpass -p vagrant ssh-copy-id -i ~/.ssh/id_rsa.pub master.lan
-
-ssh-keyscan node1.lan >> ~/.ssh/known_hosts
-sshpass -p vagrant ssh-copy-id -i ~/.ssh/id_rsa.pub node1.lan
-
-ssh-keyscan node2.lan >> ~/.ssh/known_hosts
-sshpass -p vagrant ssh-copy-id -i ~/.ssh/id_rsa.pub node2.lan
-
-ansible-playbook ~/openshift-ansible/playbooks/byo/config.yml
+ansible-playbook /home/vagrant/openshift-ansible/playbooks/byo/config.yml
