524f40af487065e5e336be22485f31f97b54f496
Kevin Eye <eye@dhcp022-108-078.wireless.buffalo.edu>
2016-03-29 14:28:48 -0400

    working!

--- provision-all.sh
+++ provision-all.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 set -ex
 
+# install basic toolset on all nodes
 yum update -y
 yum install -y wget git net-tools bind-utils iptables-services bridge-utils bash-completion docker
+
+# configure and restart docker
 echo "OPTIONS='--selinux-enabled --insecure-registry 172.30.0.0/16 --log-opt max-size=1M --log-opt max-file=3'" >> /etc/sysconfig/docker
 systemctl enable docker
 systemctl restart docker
--- provision-master.sh
+++ provision-master.sh
@@ -1,25 +1,46 @@
 #!/bin/sh
 set -ex
 
+# install ansible
 yum -y install https://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-5.noarch.rpm
 sed -i -e "s/^enabled=1/enabled=0/" /etc/yum.repos.d/epel.repo
 yum -y --enablerepo=epel install ansible pyOpenSSL
 
+# add out ansible config to system
 cp /home/vagrant/sync/ansible-hosts /etc/ansible/hosts
 
+# rest of script is as vagrant user
 su vagrant
 cd /home/vagrant
+
+# check out openshift ansible module
 git clone https://github.com/openshift/openshift-ansible
 
-# ssh-keygen -f /home/vagrant/.ssh/id_rsa -N ''
-# 
-# ssh-keyscan master.lan >> /home/vagrant/.ssh/known_hosts
-# sshpass -p vagrant ssh-copy-id -i /home/vagrant/.ssh/id_rsa.pub master.lan
-# 
-# ssh-keyscan node1.lan >> /home/vagrant/.ssh/known_hosts
-# sshpass -p vagrant ssh-copy-id -i /home/vagrant/.ssh/id_rsa.pub node1.lan
-# 
-# ssh-keyscan node2.lan >> /home/vagrant/.ssh/known_hosts
-# sshpass -p vagrant ssh-copy-id -i /home/vagrant/.ssh/id_rsa.pub node2.lan
+# generate key for ansible passwordless login
+ssh-keygen -f /home/vagrant/.ssh/id_rsa -N ''
 
-# ansible-playbook /home/vagrant/openshift-ansible/playbooks/byo/config.yml
+# if the rest of this doesn't work, it may just be that DNS takes some time to catch up
+
+# add master to known hosts and copy key file in for ansible passwordless login
+ssh-keyscan master.lan >> /home/vagrant/.ssh/known_hosts
+sshpass -p vagrant ssh-copy-id -i /home/vagrant/.ssh/id_rsa.pub master.lan
+
+# shut down vagrant's default interface (ssh port forwarding) -- it confuses openshift's networking defaults
+sudo ifdown eth0
+
+# add node1 to known hosts and copy key file in for ansible passwordless login
+ssh-keyscan node1.lan >> /home/vagrant/.ssh/known_hosts
+sshpass -p vagrant ssh-copy-id -i /home/vagrant/.ssh/id_rsa.pub node1.lan
+
+# shut down vagrant's default interface (ssh port forwarding) -- it confuses openshift's networking defaults
+ssh node1.lan sudo ifdown eth0
+
+# add node2 to known hosts and copy key file in for ansible passwordless login
+ssh-keyscan node2.lan >> /home/vagrant/.ssh/known_hosts
+sshpass -p vagrant ssh-copy-id -i /home/vagrant/.ssh/id_rsa.pub node2.lan
+
+# shut down vagrant's default interface (ssh port forwarding) -- it confuses openshift's networking defaults
+ssh node2.lan sudo ifdown eth0
+
+# run the advanced install; config is in ansible-hosts
+ansible-playbook /home/vagrant/openshift-ansible/playbooks/byo/config.yml
